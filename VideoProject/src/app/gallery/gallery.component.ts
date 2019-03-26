d62050e4be5af54e34e2645a2ec051275c58cc60
satyazmx <thakursatya6@gmail.com>
2019-03-26 08:26:30 +0530

    gallery service

--- VideoProject/src/app/gallery/gallery.component.ts
+++ VideoProject/src/app/gallery/gallery.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges } from '@angular/core';
+import { ImageService } from '../services/image.service';
+import { GalleryImage } from '../models/GalleryImage.model';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit ,OnChanges{
 
-  constructor() { }
+  images : Observable<GalleryImage[]>
+
+  constructor( private imageService : ImageService) { }
 
   ngOnInit() {
+
+    this.images = this.imageService.getImages();
+
+  }
+  ngOnChanges() {
+
+    this.images = this.imageService.getImages();
+
   }
 
 }
